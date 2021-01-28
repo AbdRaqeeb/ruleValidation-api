2d06b8553f2407779fb14bd77f56dc04e1c4972a
Abdur-Raqeeb Ajao <ajaorqb@gmail.com>
2021-01-28 21:01:36 +0100

    Chore: corrected import file

--- routes/validate.js
+++ routes/validate.js
@@ -3,7 +3,7 @@ import { validate } from '../controllers/validate.js';
 
 const router = express.Router();
 
-import { scan } from "../middleware/scan";
+import { scan } from "../middleware/scan.js";
 
 router.route('/').post(scan, validate);
 
